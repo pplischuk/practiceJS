bfef1dc24ee6523b403bc6e312040f8cb87a0848
pplischuk <pplischuk@gmail.com>
2021-02-28 18:28:23 +0200

    take off comment=)

--- script.js
+++ script.js
@@ -16,44 +16,52 @@
 // <90 - B
 // <100 - A
 
-// let arr = [80, 77, 68, 88, 95,]
-// let Newarr = [0];
-// for(let i = 0; i < arr.length; i++){
-// 	Newarr[0] = arr[i] + Newarr[0]
-// }
-// let MidlScore = Newarr[0]/arr.length;
-// alert(`MidlScore: ${MidlScore}`)
+let arr = [80, 77, 68, 88, 95,]
+let Newarr = [0];
+for(let i = 0; i < arr.length; i++){
+	Newarr[0] = arr[i] + Newarr[0]
+}
+let MidlScore = Newarr[0]/arr.length;
+alert(`MidlScore: ${MidlScore}`)
 
-// if(MidlScore < 60){
-// 	alert('Score: F')
-// }else if(MidlScore < 70){
-// 	alert('Score: D')
-// }else if(MidlScore < 80){
-// 	alert('Score: C')
-// }else if(MidlScore < 90){
-// 	alert('Score: B')
-// }else if(MidlScore < 100){
-// 	alert('Score: A')
-// }
+if(MidlScore < 60){
+	alert('Score: F')
+}else if(MidlScore < 70){
+	alert('Score: D')
+}else if(MidlScore < 80){
+	alert('Score: C')
+}else if(MidlScore < 90){
+	alert('Score: B')
+}else if(MidlScore < 100){
+	alert('Score: A')
+}
 
-// 2. Write a JavaScript program to sum the multiples of 3 or 5 under 1000.
+//2. Write a JavaScript program to sum the multiples of 3 or 5 under 1000.
 
-// let num = 0;
-// for( let i = 0; i <= 1000; i++){
-// 	if((i % 3 === 0)||(i % 5 === 0)){
-// 		num += i;
-// 	}
-// }
-// console.log(num)
+let num = 0;
+for( let i = 0; i <= 1000; i++){
+	if((i % 3 === 0)||(i % 5 === 0)){
+		num += i;
+	}
+}
+console.log(num)
 
 //3. Write a JavaScript program to construct the following pattern, using a nested for loop.
-/*
 
-*/
+* 
 
-// for( let i = 0; i < 5; i++){
-// 	for(let j = 0; j < i; j++){
-// 		document.write("* ")
-// 	}
-// 	document.write("*<br \/> <br \/>")
-// }
+* * 
+
+* * * 
+
+* * * * 
+
+* * * * *
+
+
+for( let i = 0; i < 5; i++){
+	for(let j = 0; j < i; j++){
+		document.write("* ")
+	}
+	document.write("*<br \/> <br \/>")
+}
